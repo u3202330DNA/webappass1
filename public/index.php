de9dbc933302ba8cb9f09d181ef525464bd98a4e
u3202330DNA <53715287+u3202330DNA@users.noreply.github.com>
2020-09-10 14:24:03 +1000

    extra code

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
+<?php
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
 <?php include "templates/header.php"; ?>
 
 <ul>
